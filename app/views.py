1abc72d14234b674cde2cd6e1746fc3752745fb2
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-02-06 13:01:59 +0200

    1) Make available Task view only for creators 2) Add redirect for authenticated users 3) Edit templates

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth import login
 
 from .models import Task
@@ -17,41 +17,59 @@ class HomeView(LoginRequiredMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['cnt'] = context['tasks'].count()
+        context['cnt'] = context['tasks'].filter(is_completed=False).count()
+        search_input = self.request.GET.get('search') or ''
+
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input) or context['tasks'].filter(
+                description__icontains=search_input)
+
         return context
 
 
-class TaskDetail(LoginRequiredMixin, DetailView):
+class TaskDetail(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     context_object_name = 'task'
     template_name = 'app/task_detail.html'
 
     def get_queryset(self):
         return Task.objects.filter(pk=self.kwargs['pk'])
 
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ('title', 'description', 'priority', 'until', )
+    fields = ('title', 'description', 'priority', 'until',)
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 
-class TaskUpdate(LoginRequiredMixin, UpdateView):
+class TaskUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Task
-    fields = ('title', 'description', 'priority', 'until', )
+    fields = ('title', 'description', 'priority', 'until',)
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
-class TaskDelete(LoginRequiredMixin, DeleteView):
+
+class TaskDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     template_name = 'app/task_delete.html'
     success_url = reverse_lazy('home')
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
 
 class UserRegister(FormView):
@@ -65,6 +83,11 @@ class UserRegister(FormView):
         if user is not None:
             login(self.request, user)
         return super(UserRegister, self).form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super(UserRegister, self).get(*args, **kwargs)
 
 
 def action(request, pk):
