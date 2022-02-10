f3f199a50412a08252632489144bf94067ab2504
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-02-10 22:11:24 +0200

    add allowed hosts

--- DjangoToDO/settings.py
+++ DjangoToDO/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', False)
 
